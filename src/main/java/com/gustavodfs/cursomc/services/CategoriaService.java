57febb5de7c736f5fc254e8856175043fa1730ed
Gustavodfs <gustavodfs@gmail.com>
2019-10-08 10:55:17 -0300

    Criando repository e service

---
+++ src/main/java/com/gustavodfs/cursomc/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.gustavodfs.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavodfs.cursomc.domain.Categoria;
+import com.gustavodfs.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null);
+	}
+	
+	
+	
+}
