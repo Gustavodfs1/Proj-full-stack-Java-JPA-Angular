48a7bb48e52e3e371ff75398a7fd180e91ec9fcb
Gustavodfs <gustavodfs@gmail.com>
2019-10-08 13:26:48 -0300

    ajustando endpoint

--- src/main/java/com/gustavodfs/cursomc/services/CategoriaService.java
+++ src/main/java/com/gustavodfs/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.gustavodfs.cursomc.domain.Categoria;
 import com.gustavodfs.cursomc.repositories.CategoriaRepository;
+import com.gustavodfs.cursomc.services.execptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,10 +18,10 @@ public class CategoriaService {
 	
 	
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id); 
-		return obj.orElse(null);
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	
+	
 	}
-	
-	
-	
 }
