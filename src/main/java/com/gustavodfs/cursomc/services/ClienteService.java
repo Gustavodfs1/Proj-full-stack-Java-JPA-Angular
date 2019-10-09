50a4aff5f21b78a36f6ea6602ff80a2ec2661d8a
Gustavodfs <gustavodfs@gmail.com>
2019-10-09 11:14:22 -0300

    Endpoint/Clientes

---
+++ src/main/java/com/gustavodfs/cursomc/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.gustavodfs.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavodfs.cursomc.domain.Cliente;
+import com.gustavodfs.cursomc.repositories.ClienteRepository;
+import com.gustavodfs.cursomc.services.execptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	
+	
+	}
+}
