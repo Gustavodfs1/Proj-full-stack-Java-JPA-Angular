0e4c6cff5a034d7e0596fa5363d776551468e910
Gustavodfs <gustavodfs@gmail.com>
2019-10-28 16:30:40 -0200

    PUT, DELETE e GET para Cliente

--- src/main/java/com/gustavodfs/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/gustavodfs/cursomc/repositories/ClienteRepository.java
@@ -1,9 +1,13 @@
 package com.gustavodfs.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gustavodfs.cursomc.domain.Cliente;
+import com.gustavodfs.cursomc.dto.ClienteDTO;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
--- src/main/java/com/gustavodfs/cursomc/services/ClienteService.java
+++ src/main/java/com/gustavodfs/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.gustavodfs.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gustavodfs.cursomc.domain.Cliente;
+import com.gustavodfs.cursomc.domain.Cliente;
+import com.gustavodfs.cursomc.dto.ClienteDTO;
 import com.gustavodfs.cursomc.repositories.ClienteRepository;
+import com.gustavodfs.cursomc.services.execptions.DataExceptionException;
 import com.gustavodfs.cursomc.services.execptions.ObjectNotFoundException;
 
 @Service
@@ -23,5 +31,39 @@ public class ClienteService {
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	
 	
+	}
+	public Cliente update(Cliente obj) {
+		Cliente newObj = buscar(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		buscar(id);
+		try {
+		repo.deleteById(id);
+	}catch (DataIntegrityViolationException e) {
+		throw new DataExceptionException("Não é possivel excluir porque ha entidades relacionadas");
+	}
+	}
+	
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
+	}
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj,Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
